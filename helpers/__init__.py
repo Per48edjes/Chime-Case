d19dfaa5b58334f52ba7b471f6628b35ea81987a
Ravi Dayabhai <rdayabhai@flexport.com>
2021-05-11 12:29:10 -0400

    Reduce Spend Tracker PMPM to

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -9,7 +9,7 @@ INTERCHANGE = 1.5 / 100  # per purchase dollar
 
 # Costs
 COGS = 1.05 / 100  # per purchase dollar
-SPEND_TRACKER_PMPM = 5  # cost per member-month
+SPEND_TRACKER_PMPM = 3  # cost per member-month
 
 # Profitability
 GROSS_MARGIN = INTERCHANGE - COGS
